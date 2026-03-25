7942c148737b2579b4e8184670008172330dd92a
Mbk165 <mohamedkadri165@gmail.com>
2026-03-25 09:30:21 +0100

    Modification de exo1.py pour qu'il n'y ai que agent6, modification de agent6.py pour qu'il tourne sur lui-meme

--- src/agents/team6/agent6.py
+++ src/agents/team6/agent6.py
@@ -6,13 +6,14 @@ from agents.kart_agent import KartAgent
 
 
 class Agent6(KartAgent):
+	#Agent qui tourne sur lui meme a vitesse constante
     def __init__(self, env, path_lookahead=3):
         super().__init__(env)
         self.path_lookahead = path_lookahead
         self.agent_positions = []
         self.obs = None
         self.isEnd = False
-        self.name = "Team6" # replace with your chosen name
+        self.name = "Kadri Mohamed-Bilal" # Remplacer par mon nom
 
     def reset(self):
         self.obs, _ = self.env.reset()
@@ -22,8 +23,8 @@ class Agent6(KartAgent):
         return self.isEnd
 
     def choose_action(self, obs):
-        acceleration = random.random()
-        steering = random.random()
+        acceleration = 0.1 #On mets une constante basse pour que l'agent soit assez lent pour faire un tour sur sois meme sans taper le mur
+        steering = -1 #Steer a 1 (positif) pour que l'agent tourne au maximum a droite. -1 pour tourner au maximum a gauche 
         action = {
             "acceleration": acceleration,
             "steer": steering,
