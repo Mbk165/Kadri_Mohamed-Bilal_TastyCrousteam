5784f0e9969f51e0d5bb4579647e777d950cd09f
Mbk165 <mohamedkadri165@gmail.com>
2026-03-25 10:10:10 +0100

    Ajout des commentaire pour agent7

--- src/agents/team7/agent7.py
+++ src/agents/team7/agent7.py
@@ -22,24 +22,27 @@ class Agent7(KartAgent):
         
     
     def moove_forward(self, obs, action):
+    	#Methode qui permet au kart rester au centre de la piste
     	steer = action["steer"]
-    	center = obs["paths_end"][2]
-    	if (center[2] > 30 and abs(obs["center_path_distance"]) < 3):
-    		steer = 0
-    	elif abs(center[0]) > 0.6:
-    		steer += 0.6*center[0]
-    	action["steer"] = np.clip(steer, -1, 1)
+    	center = obs["paths_end"][2] # center est le noeuds du 2eme segment apres le notre
+    	if (center[2] > 20 and abs(obs["center_path_distance"]) < 2):
+    	#On verifie que la coordonné z (front) est loin et que  notre kart est a une distance inferieur a 2 du centre de la piste 
+    		steer = 0 #on ne bouge pas le volant
+    	elif abs(center[0]) > 0.6: 
+    	#OU que la valeur absolue de la coordonné x (gauche) est inferieur 0.6
+    		steer += 0.5*center[0] #on augmente le steer de la moitié de notre coordonné x  
+    	action["steer"] = np.clip(steer, -1, 1) # on restreint le steer entre -1 et 1 par securité
     	return action
     	    	
     def choose_action(self, obs):
         action = {
-            "acceleration": 0.5,
-            "steer": 0,
+            "acceleration": 0.5, # pour que le kart avance a une vitesse constante
+            "steer": 0, # pour que des le depart le kart reste droit
             "brake": False, # bool(random.getrandbits(1)),
             "drift": bool(random.getrandbits(1)),
             "nitro": bool(random.getrandbits(1)),
             "rescue":bool(random.getrandbits(1)),
             "fire": bool(random.getrandbits(1)),
         }
-        moove_action = self.moove_forward(obs, action)
+        moove_action = self.moove_forward(obs, action) #on appel la methode moove_froward avec l'acceleration et le steer de base 
         return moove_action
